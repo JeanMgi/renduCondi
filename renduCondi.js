e0f7cb6ffd66449d0f631d982ee2cb29713bd542
JeanMgi <145458375+JeanMgi@users.noreply.github.com>
2023-09-22 16:01:14 +0200

    Add files via upload

---
+++ renduCondi.js
@@ -0,0 +1,14 @@
+"use strict";
+let a = 4;
+let b = 4;
+let c = 3;
+switch(a){
+    case b:
+        console.log("egal à b");
+        break;
+    case c:
+        console.log("egal à c");
+        break;
+    default:
+        console.log("egal à rien");
+}
